ae689a871c266bd825c6b86ca633c3af40e56b7c
Alexander Epaneshnikov <aarnaarn2@gmail.com>
2021-04-15 19:10:11 +0300

    p10k: move battery indicator

--- .config/p10k/p10k.zsh
+++ .config/p10k/p10k.zsh
@@ -48,6 +48,7 @@
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
+    battery               # internal battery
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
@@ -101,7 +102,6 @@
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
