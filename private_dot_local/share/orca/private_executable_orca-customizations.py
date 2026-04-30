c4c2051946a494dbe115cb83ea054b49e39bc1e5
Alexander Epaneshnikov <aarnaarn2@gmail.com>
2026-04-30 03:04:12 +0300

    rm orca config

--- private_dot_local/share/orca/private_executable_orca-customizations.py
+++
@@ -1,6 +0,0 @@
-import orca.settings
-
-orca.settings.speakIndentationOnlyIfChanged = True
-orca.settings.flatReviewIsRestricted = True
-orca.settings.wrappedStructuralNavigation = False
-orca.settings.enableAutoLanguageSwitching = False
